090d6c5095c5f1463195492ef0ef6d61d5cdf3a7
BorisMilanov <boris.mil.96@gmail.com>
2022-01-11 15:01:45 +0200

    Update cubeController.js

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -11,7 +11,7 @@ const getCreateCubePage = (req, res) => {
 const createCube = (req, res) => {
     let { name, description, imageUrl, difficulty } = req.body;
     cubeService.create(name, description, imageUrl, difficulty);
-    res.redirect('/cube/create');
+    res.redirect('/');
 }
 const cubeDetails = (req, res) => {
     let cube = cubeService.getOne(req.params.cubeId);
