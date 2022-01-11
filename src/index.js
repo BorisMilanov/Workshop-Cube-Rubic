7182e99c50f661e9e37c3164dc5d7d87f249b55a
BorisMilanov <boris.mil.96@gmail.com>
2022-01-11 16:31:14 +0200

    Add search functionality for cubes.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
+const config = require('./config/config.json')[process.env.NODE_ENV];
+
 const app = express();
+
 require('./config/handlebars')(app);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.use(routes);
-app.listen(3000);
+
+app.listen(config.PORT,console.log.bind(console,`Application is running on http://localhost:${config.PORT}`));
