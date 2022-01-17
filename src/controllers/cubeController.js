641e00245277f5bab65df5cc139355fdd45f9e32
BorisMilanov <boris.mil.96@gmail.com>
2022-01-17 16:11:17 +0200

    Config accessory page.
    Need fix run only man manually .

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -8,15 +8,15 @@ const getCreateCubePage = (req, res) => {
     res.render('create');
 };
 
-const createCube = async(req, res) => {
-    let { name, description, imageUrl, difficulty } = req.body
+const createCube = async (req, res) => {
+    let { name, description, imageUrl, difficulty } = req.body;
     try {
         await cubeService.create(name, description, imageUrl, difficulty);
         res.redirect('/');
-    }catch(error){
+    } catch (error) {
         res.status(400).send(error.message).end();
     }
-   
+
 }
 const cubeDetails = async (req, res) => {
     let cube = await cubeService.getOne(req.params.cubeId);
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
+const accessoryController = require('./controllers/accessoryController');
 const router = express.Router();
 
-router.use('/cube',cubeController);
-router.use('/',homeController);
-router.use('/about',homeController);
+router.use('/cube', cubeController);
+router.use('/', homeController);
+router.use('/about', homeController);
+router.use('/accessory', accessoryController);
+
 module.exports = router;
